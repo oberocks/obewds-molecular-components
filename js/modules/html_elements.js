ee6cdd4dde66933c95d6e6a3607ddc32917407a9
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2020-06-11 04:37:48 -0400

    0.5.0: added anchor element module

--- js/modules/html_elements.js
+++ js/modules/html_elements.js
@@ -1,3 +1,4 @@
+export { Anchor } from './html_elements/anchor.js';
 export { Headline } from './html_elements/headline.js';
 export { Icon } from './html_elements/icon.js';
 export { Image } from './html_elements/image.js';
---
+++ js/modules/html_elements/anchor.js
@@ -0,0 +1,47 @@
+import { merge_objects } from '../helpers/merge_objects.js';
+import { applyAttributes, insertText } from './utilities/dom_generation.js';
+
+class Anchor
+{
+    constructor (opts = false)
+    {
+        /** 
+         * Anchor : A helper script to generate an anchor link element
+         * @param {Xxxxxx} Xxxxxx : Xxxxxx
+         */
+        
+        this._defaults = {
+            attributes: {
+                href: '#'
+            },
+            text: 'Default Link'
+        };
+
+        // merge any passed options settings into the default settings to get a final settings object
+        this.defaults = (opts) ? merge_objects(true, this._defaults, opts) : this._defaults;
+
+        // clear original defaults
+        this._defaults = null;
+    }
+
+    generate (options = false)
+    {
+        // merge any passed options settings into the default settings to get a final settings object
+        let opts = (options) ? merge_objects(true, this.defaults, options) : this.defaults;
+
+        // create the element
+        let el = document.createElement('a');
+
+        // check if there are attributes then set them
+        applyAttributes(el, opts.attributes);
+
+        // check if text is a string and if so then add it as a text node
+        insertText(el, opts.text);
+
+        // return the new element node
+        return el;
+    }
+    
+}
+  
+export { Anchor };
