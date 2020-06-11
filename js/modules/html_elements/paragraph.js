bd02ed9bd2b768848023224605b20f9a044b8d8d
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2020-06-11 03:48:54 -0400

    0.3.12: refactored paragraph module

--- js/modules/html_elements/paragraph.js
+++ js/modules/html_elements/paragraph.js
@@ -1,40 +1,40 @@
+import { merge_objects } from '../helpers/merge_objects.js';
 import { applyAttributes, insertText } from './utilities/dom_generation.js';
 
 class Paragraph
 {
-    /** 
-     * Paragraph : A helper script to generate a paragraph element
-     * @param {Object} attributes  : Xxxxx
-     * @param {String, Array} text : Xxxxx
-     */
-
-    constructor (options = false)
+    constructor (opts = false)
     {
-        this.attributes = options.attributes ? options.attributes : {};
-        this.text       = options.text ? options.text : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut condimentum vitae risus vitae semper. Donec consectetur felis et mollis tristique. Nunc consequat lacus in urna congue, eu lacinia est placerat.';
-    }
+        /** 
+         * Paragraph : A helper script to generate a paragraph element
+         * @param {Xxxxxx} Xxxxxx : Xxxxxx
+         */
+        
+        this._defaults = {
+            attributes: {},
+            text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut condimentum vitae risus vitae semper. Donec consectetur felis et mollis tristique. Nunc consequat lacus in urna congue, eu lacinia est placerat.' 
+        };
 
-    /** 
-     * Generate (Paragraph) : A helper script to generate a <p> HTML element
-     * @param {Object} attributes  : Xxxxx
-     * @param {String, Array} text : Xxxxx
-     */
+        // merge any passed options settings into the default settings to get a final settings object
+        this.defaults = (opts) ? merge_objects(true, this._defaults, opts) : this._defaults;
+
+        // clear original defaults
+        this._defaults = null;
+    }
 
     generate (options = false)
     {
-        // set up any passed options and merge attributes from any passed class settings
-        let instance_attributes = options.attributes ? options.attributes : {};
-        let attributes = Object.assign(instance_attributes, this.attributes);
-        let text       = options.text ? options.text : this.text;
+        // merge any passed options settings into the default settings to get a final settings object
+        let opts = (options) ? merge_objects(true, this.defaults, options) : this.defaults;
 
         // create the element
         let el = document.createElement('p');
 
         // check if there are attributes then set them
-        applyAttributes(el, attributes);
+        applyAttributes(el, opts.attributes);
 
         // check if text is a string and if so then add it as a text node
-        insertText(el, text);
+        insertText(el, opts.text);
 
         // return the new element node
         return el;
