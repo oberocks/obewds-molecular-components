1335b0da59e8469acc27d2f157a2106e4b213a2f
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2020-08-27 16:15:05 -0400

    0.10.2: patched bug from changing var names

--- js/modules/app_data.js
+++ js/modules/app_data.js
@@ -1,19 +1,19 @@
-export { DM_BRAND } from './app_data/brand_data.js';
-export { DM_STRUCTURE } from './app_data/structure_data.js';
+export { MC_BRAND } from './app_data/brand_data.js';
+export { MC_STRUCTURE } from './app_data/structure_data.js';
 
 
-import { DM_BRAND } from './app_data/brand_data.js';
-import { DM_STRUCTURE } from './app_data/structure_data.js';
+import { MC_BRAND } from './app_data/brand_data.js';
+import { MC_STRUCTURE } from './app_data/structure_data.js';
 
 
-let DM = {};
+let MC = {};
 
 
-DM.brand = DM_BRAND;
-DM.structure = DM_STRUCTURE;
+MC.brand = MC_BRAND;
+MC.structure = MC_STRUCTURE;
 
 
-const APP_DATA = DM;
+const APP_DATA = MC;
 
 
 export { APP_DATA }
