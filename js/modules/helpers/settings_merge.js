f7859cdde26179a992286acf27e145b37f8411d7
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2020-09-04 14:52:55 -0400

    1.0.3: optimized settings_merge, removed warn loop & added console error for type issues

--- js/modules/helpers/settings_merge.js
+++ js/modules/helpers/settings_merge.js
@@ -41,129 +41,116 @@ function settings_merge (baseObj, modObj) {
     let output = {};
             
     var merge = function (baseObj, modObj) {
-    
-        // check the modObject and trigger console warnings as needed
-        for ( var prop in modObj ) {
-
-            // if a modified object's property is not already defined in the base object
-            // then log a warning for the user/dev that the data will be skipped
-            /*
-            if ( baseObj[prop] === undefined ) {
-                console.warn('OBE:WDS Molecular Components Warning: The property [ ' + prop + ' ] with a value [ ' + modObj[prop] + ' ] was ignored, because it is not a valid property for the molecular component it was passed into.' );
-            }
-            */
-            
-            // if a modified object's property value is null
-            // then log a warning for the user/dev that null values will be skipped
-            if ( Object.prototype.toString.call(modObj[prop]) === null || Object.prototype.toString.call(modObj[prop]) === 'null' || Object.prototype.toString.call(modObj[prop]) === 'NULL') {
-                console.warn('OBE:WDS Molecular Components Warning: The property [ ' + prop + ' ] was passed with a value of [ ' + modObj[prop] + ' ], so it was ignored. A null value is not not an allowed value for object properties in molecular components, so the compoent default value was used for this property instead.' );
-            }
-
-        }
         
         // loop through the properties of the base object
         for ( var prop in baseObj ) {
 
-            // if this base object property is a boolean
-            if ( is_boolean(baseObj[prop]) ) {
+            if (baseObj.hasOwnProperty(prop)) {
 
-                // init a var to help handle incorrect declarations of true/false values
-                let mod_obj_bool;
+                // if this base object property is a boolean
+                if ( is_boolean(baseObj[prop]) ) {
 
-                if ( modObj[prop] === false || modObj[prop] === 'false' || modObj[prop] === 'FALSE' ) {
-                    
-                    // correct any incorrect false values as a false boolean
-                    mod_obj_bool = false;
+                    // init a var to help handle incorrect declarations of true/false values
+                    let mod_obj_bool;
 
-                } else if ( modObj[prop] === true || modObj[prop] === 'true' || modObj[prop] === 'TRUE' ) {
-                    
-                   // correct any incorrect true values as a true boolean
-                    mod_obj_bool = true; 
+                    if ( modObj[prop] === false || modObj[prop] === 'false' || modObj[prop] === 'FALSE' ) {
+                        
+                        // correct any incorrect false values as a false boolean
+                        mod_obj_bool = false;
 
-                }
-
-                // assign the corrected bool if provided - otherwise use the default property value
-                output[prop] = (typeof modObj[prop] !== 'undefined') ? mod_obj_bool : baseObj[prop];
-
-            // if this base object property is a boolean
-            } else if ( is_string(baseObj[prop]) ) {
-
-                output[prop] = is_prop_defined(modObj[prop]) ? String(modObj[prop]) : baseObj[prop];
-
-            // otherwise if this base object prop is a number
-            } else if ( is_number(baseObj[prop]) ) {
-
-                output[prop] = is_prop_defined(modObj[prop]) ? Number(modObj[prop]) : baseObj[prop];
-
-            // or if this base object prop is a big int
-            } else if ( is_bigint(baseObj[prop]) ) {
-
-                output[prop] = is_prop_defined(modObj[prop]) ? BigInt(modObj[prop]) : baseObj[prop];
-
-            // or if this base object prop is a function
-            } else if ( is_function(baseObj[prop]) ) {
-
-                output[prop] = is_prop_defined(modObj[prop]) ? modObj[prop] : baseObj[prop];
-
-            } else if ( is_object(baseObj[prop]) ) {
-
-                output[prop] = is_prop_defined(modObj[prop]) ? settings_merge( baseObj[prop], modObj[prop] ) : baseObj[prop];
-
-            } else if ( is_array(baseObj[prop]) ) {
-
-                let temp_array = [];
-
-                if ( is_array(modObj[prop]) ) {
-
-                    for (var i = 0; i < modObj[prop].length; i++) {
-
-                        if ( is_boolean(modObj[prop][i]) ) {
-
-                            let temp_value = is_prop_defined(modObj[prop][i]) ? Boolean(modObj[prop][i]) : baseObj[prop][i];
-                            temp_array.push( temp_value );
-
-                        } else if ( is_string(modObj[prop][i]) ) {
-
-                            let temp_value = is_prop_defined(modObj[prop][i]) ? String(modObj[prop][i]) : baseObj[prop][i];
-                            temp_array.push( temp_value );
-
-                        } else if ( is_number(modObj[prop][i]) ) {
-
-                            let temp_value = is_prop_defined(modObj[prop][i]) ? Number(modObj[prop][i]) : baseObj[prop][i];
-                            temp_array.push( temp_value );
-
-                        } else if ( is_bigint(modObj[prop][i]) ) {
-
-                            let temp_value = is_prop_defined(modObj[prop][i]) ? BigInt(modObj[prop][i]) : baseObj[prop][i];
-                            temp_array.push( temp_value );
-
-                        } else if ( is_element_node(modObj[prop][i]) ) {
-
-                            temp_array.push( modObj[prop][i] );
-
-                        } else if ( is_object(modObj[prop][i]) ) {
-
-                            let temp_obj = settings_merge( baseObj[prop][0], modObj[prop][i] );
-                            let final_obj = settings_merge( modObj[prop][i], temp_obj );
-                            temp_array.push( final_obj );
-
-                        } /*else if ( is_array(modObj[prop][i]) ) {
-
-                            temp_array.push( settings_merge( baseObj[prop][i], modObj[prop][i] ) );
-
-                        }*/
+                    } else if ( modObj[prop] === true || modObj[prop] === 'true' || modObj[prop] === 'TRUE' ) {
+                        
+                    // correct any incorrect true values as a true boolean
+                        mod_obj_bool = true; 
 
                     }
 
+                    // assign the corrected bool if provided - otherwise use the default property value
+                    output[prop] = (typeof modObj[prop] !== 'undefined') ? mod_obj_bool : baseObj[prop];
+
+                // if this base object property is a boolean
+                } else if ( is_string(baseObj[prop]) ) {
+
+                    output[prop] = is_prop_defined(modObj[prop]) ? String(modObj[prop]) : baseObj[prop];
+
+                // otherwise if this base object prop is a number
+                } else if ( is_number(baseObj[prop]) ) {
+
+                    output[prop] = is_prop_defined(modObj[prop]) ? Number(modObj[prop]) : baseObj[prop];
+
+                // or if this base object prop is a big int
+                } else if ( is_bigint(baseObj[prop]) ) {
+
+                    output[prop] = is_prop_defined(modObj[prop]) ? BigInt(modObj[prop]) : baseObj[prop];
+
+                // or if this base object prop is a function
+                } else if ( is_function(baseObj[prop]) ) {
+
+                    output[prop] = is_prop_defined(modObj[prop]) ? modObj[prop] : baseObj[prop];
+
+                } else if ( is_object(baseObj[prop]) ) {
+
+                    output[prop] = is_prop_defined(modObj[prop]) ? settings_merge( baseObj[prop], modObj[prop] ) : baseObj[prop];
+
+                } else if ( is_array(baseObj[prop]) ) {
+
+                    let temp_array = [];
+
+                    if ( is_array(modObj[prop]) ) {
+
+                        for (var i = 0; i < modObj[prop].length; i++) {
+
+                            if ( is_boolean(modObj[prop][i]) ) {
+
+                                let temp_value = is_prop_defined(modObj[prop][i]) ? Boolean(modObj[prop][i]) : baseObj[prop][i];
+                                temp_array.push( temp_value );
+
+                            } else if ( is_string(modObj[prop][i]) ) {
+
+                                let temp_value = is_prop_defined(modObj[prop][i]) ? String(modObj[prop][i]) : baseObj[prop][i];
+                                temp_array.push( temp_value );
+
+                            } else if ( is_number(modObj[prop][i]) ) {
+
+                                let temp_value = is_prop_defined(modObj[prop][i]) ? Number(modObj[prop][i]) : baseObj[prop][i];
+                                temp_array.push( temp_value );
+
+                            } else if ( is_bigint(modObj[prop][i]) ) {
+
+                                let temp_value = is_prop_defined(modObj[prop][i]) ? BigInt(modObj[prop][i]) : baseObj[prop][i];
+                                temp_array.push( temp_value );
+
+                            } else if ( is_element_node(modObj[prop][i]) ) {
+
+                                temp_array.push( modObj[prop][i] );
+
+                            } else if ( is_object(modObj[prop][i]) ) {
+
+                                let temp_obj = settings_merge( baseObj[prop][0], modObj[prop][i] );
+                                let final_obj = settings_merge( modObj[prop][i], temp_obj );
+                                temp_array.push( final_obj );
+
+                            } /*else if ( is_array(modObj[prop][i]) ) {
+
+                                temp_array.push( settings_merge( baseObj[prop][i], modObj[prop][i] ) );
+
+                            }*/
+
+                        }
+
+                    } else {
+
+                        temp_array = baseObj[prop];
+
+                    }
+
+                    output[prop] = temp_array;
+
                 } else {
 
-                    temp_array = baseObj[prop];
+                    console.error('OBE:WDS Molecular Components Warning: The property [ ' + prop + ' ] was passed with a value of [ ' + modObj[prop] + ' ], which is not an data type the merge_settings() function can work with. Component settings values need to be either booleans, strings, numbers, big integers, functions, objects, arrays, or element nodes (depending on the contexts and the component).' );
 
                 }
-                
-                    
-
-                output[prop] = temp_array;
 
             }
 
