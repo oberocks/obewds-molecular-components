0117422330a52b97f9afb0e983a95a9d111347e4
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2020-09-09 22:56:55 -0400

    1.1.0: new placeholder_text functionalities - can now specify index in methods

--- js/modules/content_components/placeholder_text.js
+++ js/modules/content_components/placeholder_text.js
@@ -7,39 +7,37 @@ class Placeholder_text
     {
         // define default class settings/options
         this._defaults = {
-            placeholder_text : {
-                headlines : [
-                    'Lorem Ipsum Dolor Sit',
-                    'Quisque Feugiat Hendrerit',
-                    'Mauris Ut Nulla Id Libero',
-                    'Sed Bibendum Nisi A Est Semper'
-                ],
-                paragraphs : [
-                    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed iaculis bibendum augue, in facilisis lorem euismod fermentum. Maecenas non auctor magna, et tempor purus. Morbi et ex iaculis nunc tincidunt semper a eget dui. Nulla ac turpis id arcu cursus condimentum eget vel ante. Quisque vel malesuada sapien. Etiam non urna vitae urna iaculis rutrum non non sem.',
-                    'Quisque feugiat hendrerit ornare. Ut in magna mi. Donec pellentesque viverra lorem, id vestibulum nibh. Pellentesque egestas sit amet ante sed malesuada. Suspendisse commodo facilisis nulla, a malesuada ante accumsan convallis. Sed maximus tellus eu justo ornare, varius ullamcorper nibh scelerisque. Nulla facilisi. Quisque sed eros ex. In fringilla justo odio.',
-                    'Mauris ut nulla id libero viverra lobortis. Phasellus ut elit eu diam feugiat scelerisque. Vivamus semper nibh id turpis pharetra bibendum. In eget felis risus. Nullam at tincidunt tellus, non fermentum enim. Mauris varius suscipit lectus ac feugiat. Pellentesque pulvinar semper tempor. Vivamus ac ipsum bibendum, malesuada magna id, viverra erat. Ut aliquet neque nec hendrerit tristique.',
-                    'Sed bibendum nisi a est semper consequat. Aliquam mi neque, blandit lobortis justo sit amet, commodo consectetur sem. Donec sagittis erat quis venenatis dignissim. Duis ac iaculis leo, viverra fringilla lacus. In hac habitasse platea dictumst. Vestibulum euismod purus et tellus congue accumsan. Sed ligula libero, finibus non neque sed, semper consectetur est. Ut tincidunt, sapien aliquam varius fermentum, diam sem consequat risus, eget molestie erat ante quis erat.'
-                ],
-                quotes : [
-                    'Nulla ac turpis id arcu cursus condimentum eget vel ante.',
-                    'Suspendisse commodo facilisis nulla, a malesuada ante accumsan convallis.',
-                    'In eget felis risus. Nullam at tincidunt tellus, non fermentum enim.',
-                    'Duis ac iaculis leo, viverra fringilla lacus. In hac habitasse platea dictumst.'
-                ],
-                brands : [
-                    'Lorem Ipsum',
-                    'Suspendisse',
-                    'Nulla Ac Magna',
-                    'Aliquam Vulputate'
-                ],
-                navigation : [
-                    'Link One',
-                    'Link Two',
-                    'Link Three',
-                    'Link Four',
-                    'Link Five'
-                ]
-            }
+            headlines : [
+                'Lorem Ipsum Dolor Sit',
+                'Quisque Feugiat Hendrerit',
+                'Mauris Ut Nulla Id Libero',
+                'Sed Bibendum Nisi A Est Semper'
+            ],
+            paragraphs : [
+                'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed iaculis bibendum augue, in facilisis lorem euismod fermentum. Maecenas non auctor magna, et tempor purus. Morbi et ex iaculis nunc tincidunt semper a eget dui. Nulla ac turpis id arcu cursus condimentum eget vel ante. Quisque vel malesuada sapien. Etiam non urna vitae urna iaculis rutrum non non sem.',
+                'Quisque feugiat hendrerit ornare. Ut in magna mi. Donec pellentesque viverra lorem, id vestibulum nibh. Pellentesque egestas sit amet ante sed malesuada. Suspendisse commodo facilisis nulla, a malesuada ante accumsan convallis. Sed maximus tellus eu justo ornare, varius ullamcorper nibh scelerisque. Nulla facilisi. Quisque sed eros ex. In fringilla justo odio.',
+                'Mauris ut nulla id libero viverra lobortis. Phasellus ut elit eu diam feugiat scelerisque. Vivamus semper nibh id turpis pharetra bibendum. In eget felis risus. Nullam at tincidunt tellus, non fermentum enim. Mauris varius suscipit lectus ac feugiat. Pellentesque pulvinar semper tempor. Vivamus ac ipsum bibendum, malesuada magna id, viverra erat. Ut aliquet neque nec hendrerit tristique.',
+                'Sed bibendum nisi a est semper consequat. Aliquam mi neque, blandit lobortis justo sit amet, commodo consectetur sem. Donec sagittis erat quis venenatis dignissim. Duis ac iaculis leo, viverra fringilla lacus. In hac habitasse platea dictumst. Vestibulum euismod purus et tellus congue accumsan. Sed ligula libero, finibus non neque sed, semper consectetur est. Ut tincidunt, sapien aliquam varius fermentum, diam sem consequat risus, eget molestie erat ante quis erat.'
+            ],
+            quotes : [
+                'Nulla ac turpis id arcu cursus condimentum eget vel ante.',
+                'Suspendisse commodo facilisis nulla, a malesuada ante accumsan convallis.',
+                'In eget felis risus. Nullam at tincidunt tellus, non fermentum enim.',
+                'Duis ac iaculis leo, viverra fringilla lacus. In hac habitasse platea dictumst.'
+            ],
+            brands : [
+                'Lorem Ipsum',
+                'Suspendisse',
+                'Nulla Ac Magna',
+                'Aliquam Vulputate'
+            ],
+            navigation : [
+                'Link One',
+                'Link Two',
+                'Link Three',
+                'Link Four',
+                'Link Five'
+            ]
         };
 
         // merge any passed options settings into the default settings to get a final settings object
@@ -53,68 +51,68 @@ class Placeholder_text
         return this.defaults;
     }
 
-    headline ()
+    headline (index = false)
     {
         // get a random index and return placeholder text string
-        let index = get_random_index(this.defaults.placeholder_text.headlines.length);
-        return this.defaults.placeholder_text.headlines[index];
+        let i = index ? Number(index) : get_random_index(this.defaults.headlines.length);
+        return this.defaults.headlines[i];
     }
 
-    paragraph ()
+    paragraph (index = false)
     {
         // get a random index and return placeholder text string
-        let index = get_random_index(this.defaults.placeholder_text.paragraphs.length);
-        return this.defaults.placeholder_text.paragraphs[index];
+        let i = index ? Number(index) : get_random_index(this.defaults.paragraphs.length);
+        return this.defaults.paragraphs[i];
     }
 
-    quote ()
+    quote (index = false)
     {
         // get a random index and return placeholder text string
-        let index = get_random_index(this.defaults.placeholder_text.quotes.length);
-        return this.defaults.placeholder_text.quotes[index];
+        let i = index ? Number(index) : get_random_index(this.defaults.quotes.length);
+        return this.defaults.quotes[i];
     }
 
-    brand ()
+    brand (index = false)
     {
         // get a random index and return placeholder text string
-        let index = get_random_index(this.defaults.placeholder_text.brands.length);
-        return this.defaults.placeholder_text.brands[index];
+        let i = index ? Number(index) : get_random_index(this.defaults.brands.length);
+        return this.defaults.brands[i];
     }
 
-    navigation ()
+    navigation (index = false)
     {
         // get a random index and return placeholder text string
-        let index = get_random_index(this.defaults.placeholder_text.navigation.length);
-        return this.defaults.placeholder_text.navigation[index];
+        let i = index ? Number(index) : get_random_index(this.defaults.navigation.length);
+        return this.defaults.navigation[i];
     }
 
-    node (type)
+    node (type, index = false)
     {
         let text;
         if (type === 'headline')
         {
             // 
-            text = this.headline();
+            text = index ? this.headline(index) : this.headline();
         }
         else if (type === 'paragraph')
         {
             // 
-            text = this.paragraph();
+            text = index ? this.paragraph(index) : this.paragraph();
         }
         else if (type === 'quote')
         {
             // 
-            text = this.quote();
+            text = index ? this.quote(index) : this.quote();
         }
         else if (type === 'brand')
         {
             // 
-            text = this.brand();
+            text = index ? this.brand(index) : this.brand();
         }
         else if (type === 'navigation')
         {
             // 
-            text = this.navigation();
+            text = index ? this.navigation(index) : this.navigation();
         }
 
         return document.createTextNode( text );
