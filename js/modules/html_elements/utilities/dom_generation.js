77840eddda3c3c0fdce211c97e32e05724b00751
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2020-09-30 18:32:16 -0400

    2.14.3: cleaned up syntax

--- js/modules/html_elements/utilities/dom_generation.js
+++ js/modules/html_elements/utilities/dom_generation.js
@@ -1,47 +1,55 @@
-export function apply_attributes (node, obj)
-{
-    if (Object.keys(obj).length > 0)
-    {
-        for (const key in obj)
-        {
-            if (obj[key] != null && obj[key] != false) {
+export function apply_attributes (node, obj) {
+
+    if (Object.keys(obj).length > 0) {
+
+        for (const key in obj) {
+
+            if (obj[key] !== null && obj[key] !== false) {
 
                 node.setAttribute(key, obj[key]);
 
             }
             
         }
+
     }
+
 }
 
-export function insert_text (node, str)
-{
-    if (typeof str === 'string' && str.length > 0)
-    {
+export function insert_text (node, str) {
+
+    if (typeof str === 'string' && str.length > 0) {
+
         let txt = document.createTextNode(str);
         node.appendChild(txt);
-    }
-    else if (Array.isArray(str) && str.length > 0)
-    {
-        for (var i = 0; i < str.length; i++)
-        {
-            if (str[i] instanceof Element || str[i] instanceof HTMLDocument)
-            {
+
+    } else if (Array.isArray(str) && str.length > 0) {
+
+        for (var i = 0; i < str.length; i++) {
+
+            if (str[i] instanceof Element || str[i] instanceof HTMLDocument) {
+
                 node.appendChild(str[i]);
-            }
-            else if (typeof str[i] === 'string' || str[i] instanceof String)
-            {
+
+            } else if (typeof str[i] === 'string' || str[i] instanceof String) {
+
                 let txt = document.createTextNode(str[i]);
                 node.appendChild(txt);
+
             }
+
         }
+
     }
+
 }
 
-export function append_child (node, childNodes)
-{
-    if (childNodes && childNodes.nodeType === Node.ELEMENT_NODE)
-    {
+export function append_child (node, childNodes) {
+
+    if (childNodes && childNodes.nodeType === Node.ELEMENT_NODE) {
+
         node.appendChild(childNodes);
+
     }
+
 }
