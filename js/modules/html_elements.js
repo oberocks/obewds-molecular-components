18bb739be87c398389913705119d1398806ec079
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2020-10-15 23:53:11 -0400

    2.28.0: added Address, Br, & Strong classes to html elems

--- js/modules/html_elements.js
+++ js/modules/html_elements.js
@@ -1,5 +1,7 @@
+export { Address } from './html_elements/Address.js';
 export { Anchor } from './html_elements/Anchor.js';
 export { Article } from './html_elements/Article.js';
+export { Br } from './html_elements/Br.js';
 export { Cite } from './html_elements/Cite.js';
 export { Div } from './html_elements/Div.js';
 export { Form } from './html_elements/Form.js';
@@ -11,4 +13,5 @@ export { Img } from './html_elements/Img.js';
 export { Main } from './html_elements/Main.js';
 export { Paragraph } from './html_elements/Paragraph.js';
 export { Section } from './html_elements/Section.js';
+export { Strong } from './html_elements/Strong.js';
 export { Style } from './html_elements/Style.js';
---
+++ js/modules/html_elements/Strong.js
@@ -0,0 +1,59 @@
+import { Html_element } from './data/Html_element.js';
+import { apply_attributes, insert_text } from './utilities/dom_generation.js';
+import { settings_merge } from '../helpers/settings_merge.js';
+
+export class Strong extends Html_element {
+
+    constructor (opts = false) {
+
+        // get props from inhereted class
+        super();
+
+        // merge any passed options settings into the default settings to get a final settings object
+        this.defaults = (opts) ? settings_merge(this._defaults, opts) : this._defaults;
+
+        // clear original defaults
+        this._defaults = null;
+
+    }
+
+    get_class_defaults () {
+        
+        return this.defaults;
+    
+    }
+
+    get_generate_options (options) {
+        
+        return settings_merge(this.defaults, options);
+    
+    }
+
+    generate (options = false) {
+
+        // merge any passed options settings into the default settings to get a final settings object
+        let opts = (options) ? settings_merge(this.defaults, options) : this.defaults;
+
+        // create the element
+        let el = document.createElement('strong');
+
+        // check if there are attributes then set them
+        apply_attributes(el, opts.attributes);
+
+        // check if text is a string and if so then add it as a text node
+        insert_text(el, opts.text);
+
+        // return the new element node
+        return el;
+
+    }
+
+    example () {
+        
+        return this.generate({
+            text : '<strong>'
+        });
+    
+    }
+    
+}
