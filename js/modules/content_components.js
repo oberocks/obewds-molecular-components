cad1e47225704ac3f383f2d23e3f7109006722df
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2020-10-16 17:33:54 -0400

    2.29.1: updated content component paths for new capitalized class filenames

--- js/modules/content_components.js
+++ js/modules/content_components.js
@@ -1,5 +1,5 @@
-export { Blockquote } from './content_components/blockquote.js';
-export { Blockquote_with_footer } from './content_components/blockquote_with_footer.js';
-export { Headline_group } from './content_components/headline_group.js';
-export { Placeholder_text } from './content_components/placeholder_text.js';
-export { Video_component } from './content_components/video_component.js';
+export { Blockquote } from './content_components/Blockquote.js';
+export { Blockquote_with_footer } from './content_components/Blockquote_with_footer.js';
+export { Headline_group } from './content_components/Headline_group.js';
+export { Placeholder_text } from './content_components/Placeholder_text.js';
+export { Video_component } from './content_components/Video_component.js';
