8cb9a94938d1433da5e82d57ff84bd28f8066684
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2021-01-02 21:53:20 -0500

    2.30.7: adding ../modules/html_elements/data/Html_elements.js back in

---
+++ js/modules/html_elements/data/Html_elements.js
@@ -0,0 +1,39 @@
+export class Html_element {
+
+    constructor () {
+
+        // define default class settings/options
+        this._defaults = {
+            attributes : {
+                // REF: https://developer.mozilla.org/en-US/docs/Web/HTML/Global_attributes
+                accesskey : null,
+                autocapitalize : null,
+                class : null,
+                contenteditable : null,
+                dir : null,
+                draggable : null,
+                exportparts : null,
+                hidden : null,
+                id : null,
+                inputmode : null,
+                is : null,
+                itemid : null,
+                itemprop : null,
+                itemref : null,
+                itemscope : null,
+                itemtype : null,
+                lang : null,
+                part : null,
+                slot : null,
+                spellcheck : null,
+                style : null,
+                tabindex : null,
+                title : null,
+                translate : null
+            },
+            text : null
+        };
+
+    }
+
+}
